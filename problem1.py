8ff4a9214518781418d657b8814f5568a0b17842
eberjoe <eberjoe@gmail.com>
2019-01-04 20:56:13 -0200

    Change naming convention

--- problem1.py
+++
@@ -1,4 +0,0 @@
-j = 0
-for i in range (3,1000):
-    j = j + i * ((i%3 * i%5) == 0)
-print(j)
--- problem2.py
+++
@@ -1,5 +0,0 @@
-a, b, c = 0, 1, 0
-while a <= 4000000:
-    c = c + (a%2 == 0)*a
-    a, b = b, a+b
-print(c)
