02b954bfe9dafdab6c3e224bfdb2458c8491df26
eberjoe <eberjoe@gmail.com>
2019-01-14 23:46:48 -0200

    Add solution 15

---
+++ problem015.py
@@ -0,0 +1,13 @@
+# Laticce paths for an 'n'x'n' grid
+
+n = 20
+
+def factorial(x):
+    for i in range(1, x):
+        x *= i
+    return x
+
+def binomialCoef(x, y):
+    return int(factorial(x)/(factorial(y)*factorial(x-y)))
+
+print(binomialCoef(n*2, n))
